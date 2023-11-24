652036ce1fb537852fb25eb5a93f45a649909023
Celeste Tanaka <cehmayumi@Gmail.com>
2023-11-24 09:47:42 -0300

    Alterado a estrutura de inserção de dadados, organização de codigo e if/else para os menus

--- listas.py
+++ listas.py
@@ -7,19 +7,13 @@ lista_unidades = ['Santo André',
                   'Itu']
 
 # LISTA DE LEMBRETES
-lista_lembretes = ['Omeprazol: 1 comprimido pela manhã.',
-                   'Remarcar o hemograma (urgente).']
+lista_lembretes = []
 
 # LISTA DE REMÉDIOS
-lista_remedios = ['Omeprazol',
-                  'Nimesulida',
-                  'Prednisolona']
+lista_remedios = []
 
 # LISTA DE CONSULTAS
-lista_consultas = ['OTORRINOLARINGOLOGISTA - Sexta-feira (24/11) às 15:00h',
-                   'ONCOLOGISTA - Terça-feira (28/11) às 13:00h']
+lista_consultas = []
 
 # LISTA DE ANOTAÇÕES/NOTAS
-lista_notas = ['23/11/2023 > Hoje senti tonturas, mas estão melhorando, acho que foi uma crise de enxaqueca',
-               '04/12/2023 > Os remédios que preciso tomar são horríveis, mas preciso deles...',
-               '24/12/2023 > Finalmente chegou o dia da ceia de Natal, esperei o mês todo por isso hahaha']
+lista_notas = []
--- main.py
+++ main.py
@@ -2,94 +2,180 @@ from listas import lista_unidades, lista_lembretes, lista_remedios, lista_consul
 
 
 # FUNÇÃO "EMERGÊNCIA": escolha do endereço para a solicitação de uma emergência.
-def emergencia():
-    print("\n=== EMERGÊNCIA ===\n")
-    print("1. Localização atual")
-    print("2. Endereço padrão")
-    print("3. Novo endereço\n")
-    opcao_emergencia = int(input("\nSelecione uma das opções acima: "))
 
-    if opcao_emergencia == 1 or opcao_emergencia == 2:
-                print("\nConcluído. Estamos a caminho.")
-    elif opcao_emergencia == 3:
-                novo_endereco = input("Informe o novo endereço: ")
-                print("Concluído. Estamos a caminho.")
-    else:
-        print("ERRO")
+def emergencia():
+    while True:
+        print("\n=== EMERGÊNCIA ===\n")
+        print("1. Localização atual")
+        print("2. Endereço padrão")
+        print("3. Novo endereço")
+        print("4. Voltar\n")
+
+        opcao_emergencia = int(input("Selecione uma das opções acima: "))
+
+        if opcao_emergencia == 1 or opcao_emergencia == 2:
+            print("\nConcluído. Estamos a caminho.")
+            break
+
+        elif opcao_emergencia == 3:
+            novo_endereco = input("Informe o novo endereço: ")
+            print("\nConcluído. Estamos a caminho.")
+            break
+
+        elif opcao_emergencia == 4:
+            break
+        else:
+            print("ERRO")
+
 
 
 # FUNÇÃO "DIÁRIO": para que o paciente mantenha controle das próprias informações.
+
 def diario():
-    print("\n=== DIÁRIO ===\n")
-    print("1. Lembretes")
-    print("2. Remédios")
-    print("3. Consultas")
-    print("4. Anotações\n")
-    opcao_diario = int(input("Selecione uma das opções acima: \n"))
+    while True:
+        print("\n=== DIÁRIO ===\n")
+        print("1. Lembretes")
+        print("2. Remédios")
+        print("3. Consultas")
+        print("4. Anotações")
+        print("5. Voltar\n")
 
-    if opcao_diario == 1:
-        print("1. Meus lembretes")
-        print("2. Adicionar lembrete")
-        opcao_lembretes = int(input("Selecione uma das opções acima: \n"))
+        opcao_diario = int(input("Selecione uma das opções acima: \n"))
 
-        if opcao_lembretes == 1:
-            print(lista_lembretes)
-        elif opcao_lembretes == 2:
-            novo_lembrete = input("Novo lembrete: ")
+        if opcao_diario == 1:
+            while True:
+                print("1. Meus lembretes")
+                print("2. Adicionar lembrete")
+                print("3. Voltar\n")
+                opcao_lembretes = int(input("Selecione uma das opções acima: \n"))
+
+                if opcao_lembretes == 1:
+                    if not lista_lembretes:
+                        print("\nNão há lembretes.")
+                        break
+
+                    else:
+                        print(lista_lembretes)
+                        break
+
+                elif opcao_lembretes == 2:
+                    novo_lembrete = input("Novo lembrete: ")
+                    lista_lembretes.append(novo_lembrete)
+                    print("\nLembrete adicionado com sucesso!")
+                    break
+
+                elif opcao_lembretes == 3:
+                    break
+
+                else:
+                    print("Opção inválida")
+
+        elif opcao_diario == 2:
+            while True:
+                print("1. Meus remédios")
+                print("2. Cadastrar novo remédio")
+                print("3. Voltar\n")
+
+                opcao_remedios = int(input("Selecione uma das opções acima: \n"))
+
+                if opcao_remedios == 1:
+                    if not lista_remedios:
+                        print("\nNão há remédios cadastrados")
+                        break
+
+                    else:
+                        print(lista_remedios)
+                        break
+
+                elif opcao_remedios == 2:
+                    novo_remedio = input("\nNovo remédio: ")
+                    lista_remedios.append(novo_remedio)
+                    print(f'\nAdicionado um novo remédio. ({novo_remedio})')
+                    break
+
+                elif opcao_remedios == 3:
+                    break
+
+                else:
+                    print("Opção inválida")
+
+        elif opcao_diario == 3:
+            while True:
+                print("1. Minhas consultas")
+                print("2. Agendar nova consulta")
+                print("3. Voltar\n")
+
+                opcao_consultas = int(input("Selecione uma das opções acima: \n"))
+
+                if opcao_consultas == 1:
+                    if not lista_consultas:
+                        print("\nNão há consultas marcadas")
+                        break
+
+                    else:
+                        print(lista_consultas)
+                        break
+
+                elif opcao_consultas == 2:
+                        nova_consulta = input("\nNova consulta: ")
+                        lista_consultas.append(nova_consulta)
+                        print("\nConsulta agendada com sucesso!")
+                        break
+                
+                elif opcao_consultas == 3:
+                    break
+
+                else:
+                    print("Opção inválida")
+
+        elif opcao_diario == 4:
+            while True:
+                print("1. Minhas anotações")
+                print("2. Nova nota")
+                print("3. Voltar\n")
+
+                opcao_notas = int(input("Selecione uma das opções acima: \n"))
+
+                if opcao_notas == 1:
+                    if not lista_notas:
+                        print("\nNão há anotações.")
+                        break
+
+                    else:
+                        print(lista_notas)
+                        break
+
+                elif opcao_notas == 2:
+                        nova_nota = input("Nova nota: ")
+                        lista_notas.append(nova_nota)
+                        print("\nAnotação salva com sucesso!")
+                        break
+                
+                elif opcao_notas == 3:
+                    break
+
+                else:
+                    print("Opção inválida")
         else:
-            print("Opção inválida")
-
-    elif opcao_diario == 2:
-        print("1. Meus remédios")
-        print("2. Cadastrar novo remédio")
-        opcao_remedios = int(input("Selecione uma das opções acima: \n"))
-
-        if opcao_remedios == 1:
-            print(lista_remedios)
-        elif opcao_remedios == 2:
-            novo_remedio = input("Novo remédio: ")
-        else:
-            print("Opção inválida")
-
-    elif opcao_diario == 3:
-        print("1. Minhas consultas")
-        print("2. Agendar nova consulta")
-        opcao_consultas = int(input("Selecione uma das opções acima: \n"))
-
-        if opcao_consultas == 1:
-                print(lista_consultas)
-        elif opcao_consultas == 2:
-                nova_consulta = input("Nova consulta: ")
-        else:
-                print("Opção inválida")
-
-    elif opcao_diario == 4:
-        print("1. Minhas anotações")
-        print("2. Nova nota")
-        opcao_notas = int(input("Selecione uma das opções acima: \n"))
-
-        if opcao_notas == 1:
-                print(lista_notas)
-        elif opcao_notas == 2:
-                nova_nota = input("Nova nota: ")
-        else:
-                print("Opção inválida")
-    else:
-        print("ERRO")
+            print("ERRO")
 
 
 # FUNÇÃO "TRIAGEM": benefício para atendimento mais rápido no pronto-socorro
-def triagem():
-    print("\n=== PRONTO-SOCORRO ===\n")
-    print("Deseja prosseguir com a triagem?")
-    opcao_triagem = input("    SIM | NÃO    \n").upper()
 
-    if opcao_triagem == "SIM":
-        print("Selecione uma das unidades abaixo: \n")
-        print(lista_unidades)
-        unidade_triagem = input("")
-        sintomas = input("Descreva o que está sentindo: ")
-        print("\nEncaminhado para a unidade selecionada.\n")
+def triagem():
+    while True:
+        print("\n=== PRONTO-SOCORRO ===\n")
+
+        print("Deseja prosseguir com a triagem?")
+        
+        opcao_triagem = input("    SIM | NÃO    \n").upper()
+
+        if opcao_triagem == "SIM" or "S":
+            print("Selecione uma das unidades abaixo: \n")
+            print(lista_unidades)
+            unidade_triagem = input("")
+            sintomas = input("Descreva o que está sentindo: ")
+            print("\nEncaminhado para a unidade selecionada.\n")
 
 
 # MENU GERAL
@@ -99,30 +185,16 @@ while True:
     print("1. Emergência")
     print("2. Diário")
     print("3. Pronto Socorro")
-    print("4. Sair")
+    print("4. Sair\n")
     acesso = int(input("Digite a opção desejada: "))
 
+
     if acesso == 1:
-        while True:
-            emergencia()
-            sair = input("\nVoltar ao menu principal?\n [SIM/NAO] ").upper()
-            if sair != "NAO":
-                break
-
+        emergencia()
     elif acesso == 2:
-        while True:
-            diario()
-            sair = input("\nVoltar ao menu principal?\n [SIM/NAO] ").upper()
-            if sair != "NAO":
-                break
-
+        diario()
     elif acesso == 3:
-        while True:
-            triagem()
-            sair = input("\nVoltar ao menu principal?\n [SIM/NAO] ").upper()
-            if sair != "NAO":
-                break
-
+        triagem()
     elif acesso == 4:
         break
 
